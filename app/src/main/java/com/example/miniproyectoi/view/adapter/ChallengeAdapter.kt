b58a54f9a15eb8b745ab62838ca8ec786497b96d
Juan Rubio <jua0729.jr@gmail.com>
2024-10-30 11:40:40 -0500

    d

---
+++ app/src/main/java/com/example/miniproyectoi/view/adapter/ChallengeAdapter.kt
@@ -0,0 +1,12 @@
+package com.example.miniproyectoi.view.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.navigation.NavController
+import androidx.recyclerview.widget.RecyclerView
+
+import com.example.miniproyectoi.model.Challenge
+
+
+class ChallengeAdapter {
+}
