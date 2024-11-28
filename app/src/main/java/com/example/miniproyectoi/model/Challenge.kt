513c4ee128febf0fd2bde2dc9761fc03a0868db4
Frankdvop <franklin.aguirre@correounivalle.edu.co>
2024-11-28 13:04:30 -0500

    Se corrige el modelo del challenge que modifique por error

--- app/src/main/java/com/example/miniproyectoi/model/Challenge.kt
+++ app/src/main/java/com/example/miniproyectoi/model/Challenge.kt
@@ -1,10 +1,10 @@
 package com.example.miniproyectoi.model
 
-import androidx.room.Entity
+
 import com.google.firebase.firestore.DocumentId
 import java.io.Serializable
 
-@Entity(tableName = "challenge")
+
 data class Challenge(
     @DocumentId
     val id: String = "",
